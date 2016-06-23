d1a3472cc8ab443347dcb6c8d5a2593482e90559
Idan Ratzabi <idanr@any.do>
2016-06-23 13:55:46 +0300

    Adding facts view

--- src/components/FactsView.js
+++ src/components/FactsView.js
@@ -8,25 +8,62 @@ import {
     StyleSheet,
     View,
     Image,
-
+    Text,
 } from 'react-native'
 
-export default class FactsView extends React.Component {
+var imageSrc = require('.././assets/fun_fact.png')
+
+var FactsView = React.createClass({
+
+    getDefaultProps() {
+        return {
+            title: "Fun Fact! sdsdsds",
+            body: "Some data about the fact",
+        };
+    },
+
+    propTypes: {
+        title: React.PropTypes.string,
+        body : React.PropTypes.string,
+        factImageUrl : React.PropTypes.string,
+    },
 
     render() {
         return (
             <View style={styles.container}>
+                 <Text style={styles.title}> {this.props.title}</Text>
+                <View style={styles.bodyContainer}>
+                    <Image source={require('.././assets/fun_fact.png')} style={styles.image}/>
+                    <Text style={styles.body}> {this.props.body} </Text>
+                </View>
             </View>
         );
     }
-}
-
-GifView.propTypes = {
-
-}
+});
 
 var styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+    },
+    bodyContainer:{
+        flex: 1,
+        flexDirection: 'row',
+        flexWrap: 'wrap'
+    },
+    title: {
+        color: 'red',
+        fontSize : 20,
+        marginTop : 30,
+        marginBottom : 30,
+    },
+    body :{
+        flex:3
+    },
+    image :{
+        flex:1,
+        width : 50,
+        height : 50,
     }
 });
+
+module.exports = FactsView;

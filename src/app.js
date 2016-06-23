21e32dbb3a9ba977488b3ec602faaa652296bdc8
talc <tal.cohen@wearetv.com>
2016-06-23 11:36:04 +0300

    remove react page

--- src/app.js
+++ src/app.js
@@ -49,16 +49,6 @@ class SpotBeat extends Component {
     return (
       <Provider store={store}>
           <View style={styles.container}>
-            <Text style={styles.welcome}>
-              Welcome to React Native!
-            </Text>
-            <Text style={styles.instructions}>
-              To get started, edit index.ios.js
-            </Text>
-            <Text style={styles.instructions}>
-              Press Cmd+R to reload,{'\n'}
-              Cmd+D or shake for dev menu
-            </Text>
             <Main/>
           </View>
        </Provider>
